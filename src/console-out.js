a0e69b775e39641930322a887b7b0a6024526ac4
Kristian Mandrup <kmandrup@gmail.com>
2017-05-08 14:32:11 +0200

    allow general inputs

--- src/console-out.js
+++ src/console-out.js
@@ -4,24 +4,50 @@ var chalk = require('chalk');
 var nodePlop = require('node-plop');
 var fs = require('fs');
 
-module.exports = (function () {
+const noop = () => {}
+
+const createLogger = (opts = {}) => {
+	var io = opts.io || console
+	return (...msg) => io.log(...msg)
+}
+
+const logger = (opts = {}) => {
+	return opts.log ? createLogger(opts) : noop
+}
+
+module.exports = (function (opts) {
+	const log = logger(opts)
 
 	function chooseOptionFromList(plopList) {
 		const plop = nodePlop();
+		const choices = plopList.map(function (p) {
+			return {
+				name: p.name + chalk.gray(!!p.description ? ' - ' + p.description : ''),
+				value: p.name
+			};
+		})
+		if (!choices || choices.length < 1) {
+			throw new Error("No generators registered")
+		}
+		log('generators', choices)
+
 		const generator = plop.setGenerator('choose', {
 			prompts: [{
 				type: 'list',
 				name: 'generator',
 				message: '[PLOP]'.blue + ' Please choose a generator.',
-				choices: plopList.map(function (p) {
-					return {
-						name: p.name + chalk.gray(!!p.description ? ' - ' + p.description : ''),
-						value: p.name
-					};
-				})
+				choices
 			}]
 		});
-		return generator.runPrompts().then(results => results.generator);
+		log('runInputs')
+		// don't ask when only 1 generator
+		if (choices.length === 1) {
+			return new Promise((resolve, reject) => {
+				resolve(choices[0].value)
+			})
+		} else {
+			return generator.runPrompts().then(results => results.generator);
+		}
 	}
 
 	function displayHelpScreen() {
@@ -43,7 +69,7 @@ module.exports = (function () {
 		);
 	}
 
-	function createInitPlopfile(cwd, callback){
+	function createInitPlopfile(cwd, callback) {
 		var initString = 'module.exports = function (plop) {\n\n' +
 			'\tplop.setGenerator(\'basics\', {\n' +
 			'\t\tdescription: \'this is a skeleton plopfile\',\n' +
--- src/plop.js
+++ src/plop.js
@@ -37,8 +37,8 @@ function run(env) {
 
 	// handle request for initializing a new plopfile
 	if (argv.init || argv.i) {
-		return out.createInitPlopfile(env.cwd, function(err){
-			if (err){
+		return out.createInitPlopfile(env.cwd, function (err) {
+			if (err) {
 				console.log(err);
 				process.exit(1);
 			}
@@ -70,9 +70,13 @@ function run(env) {
 	generators = plop.getGeneratorList();
 	if (!generator) {
 		out.chooseOptionFromList(generators).then(function (generatorName) {
+			console.log('get generator', generatorName)
 			doThePlop(plop.getGenerator(generatorName));
 		});
-	} else if (generators.map(function (v) { return v.name; }).indexOf(generator) > -1) {
+	} else if (generators.map(function (v) {
+			console.log('plop generator', v.name)
+			return v.name;
+		}).indexOf(generator) > -1) {
 		doThePlop(plop.getGenerator(generator));
 	} else {
 		console.error(chalk.red('[PLOP] ') + 'Generator "' + generator + '" not found in plopfile');
@@ -85,15 +89,19 @@ function run(env) {
 // everybody to the plop!
 //
 function doThePlop(generator) {
-	generator.runPrompts().then(generator.runActions)
+	generator.runInputs().then(generator.runActions)
 		.then(function (result) {
-			result.changes.forEach(function(line) {
+			result.changes.forEach(function (line) {
 				console.log(chalk.green('[SUCCESS]'), line.type, line.path);
 			});
 			result.failures.forEach(function (line) {
 				var logs = [chalk.red('[FAILED]')];
-				if (line.type) { logs.push(line.type); }
-				if (line.path) { logs.push(line.path); }
+				if (line.type) {
+					logs.push(line.type);
+				}
+				if (line.path) {
+					logs.push(line.path);
+				}
 
 				var error = line.error || line.message;
 				logs.push(chalk.red(error));
